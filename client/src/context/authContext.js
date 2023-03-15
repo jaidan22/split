0fa5236014c5c2c5d4e0f8703359f46fc08be596
jaidan22 <jaidansleeba22@gmail.com>
2023-03-15 21:02:10 +0530

    updated context api

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -7,23 +7,30 @@ export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
     JSON.parse(localStorage.getItem("user")) || null
   );
+  const [loading, setLoading] = useState(false);
 
   const login = async (inputs) => {
-    const res = await axios.post(
-      `${process.env.REACT_APP_API_URL}/auth/login`,
-      inputs,
-      {
-        withCredentials: true,
-      }
-    );
-
-    setCurrentUser(res.data);
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/auth/login`,
+        inputs,
+        {
+          withCredentials: true,
+        }
+      );
+      setCurrentUser(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
   };
 
   const logout = async () => {
+    setLoading(true);
     await axios.post(`${process.env.REACT_APP_API_URL}/auth/logout`);
-
     setCurrentUser(null);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -31,7 +38,9 @@ export const AuthContextProvider = ({ children }) => {
   }, [currentUser]);
 
   return (
-    <AuthContext.Provider value={{ currentUser, login, logout }}>
+    <AuthContext.Provider
+      value={{ currentUser, login, logout, loading, setLoading }}
+    >
       {children}
     </AuthContext.Provider>
   );
