31ca941a4289277cadb12490707bb5d7ff2ef63a
jaidan22 <jaidansleeba22@gmail.com>
2023-03-15 21:02:42 +0530

    sidebar and loading page

---
+++ client/src/components/loading/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loading.scss";
+
+const Loading = () => {
+  return (
+    <div class="loader">
+      <span class="loader__element"></span>
+      <span class="loader__element"></span>
+      <span class="loader__element"></span>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ client/src/components/sidebar/Sidebar.js
@@ -0,0 +1,63 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import "./sidebar.scss";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+import LogoutIcon from "@mui/icons-material/Logout";
+import HorizontalSplitIcon from "@mui/icons-material/HorizontalSplit";
+import GroupsIcon from "@mui/icons-material/Groups";
+import HelpIcon from '@mui/icons-material/Help';
+
+const Sidebar = () => {
+  const { logout } = useContext(AuthContext);
+
+  return (
+    <div className="sidebar bg-white sm:my-8 mx-10 sm:w-2/12 sm:block hidden rounded-lg px-2 relative">
+      <div className="heading px-3 py-4 pt-8">
+        <h1 className="font-extrabold text-4xl text-left flex items-center ">
+          <HorizontalSplitIcon className="mr-2" /> split
+        </h1>
+      </div>
+      <nav className="flex flex-col sm:justify-center">
+        <a href="/" className=" hover:text-slate-900">
+          <div className="navLink flex rounded-lg px-3 py-2 w-full hover:bg-slate-100 hover:text-slate-900 items-center">
+            <HomeIcon />
+            <span className="ml-2 text-slate-700 font-medium">Home</span>
+          </div>
+        </a>
+
+        <a href="/users" className=" hover:text-slate-900">
+          <div className="navLink flex rounded-lg px-3 py-2 w-full hover:bg-slate-100 hover:text-slate-900 items-center">
+            <PeopleIcon />
+            <span className="ml-2 text-slate-700 font-medium">Users</span>
+          </div>
+        </a>
+
+        <a href="/groups" className=" hover:text-slate-900">
+          <div className="navLink flex rounded-lg px-3 py-2 w-full hover:bg-slate-100 hover:text-slate-900 items-center">
+            <GroupsIcon />
+            <span className="ml-2 text-slate-700 font-medium">Groups</span>
+          </div>
+        </a>
+      </nav>
+
+      <div className="lower absolute bottom-12 left-0 w-full">
+        <button className="hover:text-slate-900 w-full" onClick={logout}>
+          <div className="navLink flex rounded-lg px-4 py-2 w-full hover:bg-slate-100 hover:text-slate-900 items-center">
+            <LogoutIcon />
+            <span className="ml-2 text-slate-700 font-medium ">Log Out</span>
+          </div>
+        </button>
+
+        <a href="/help" className=" hover:text-slate-900">
+          <div className="navLink flex rounded-lg px-4 py-2 w-full hover:bg-slate-100 hover:text-slate-900 items-center">
+            <HelpIcon />
+            <span className="ml-2 text-slate-700 font-medium">Help</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
