811873b11bf6f1579ac90221f3ea16b1936ce7cd
jaidan22 <jaidansleeba22@gmail.com>
2023-03-16 21:50:22 +0530

    new routes and axios instance

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,14 @@ import Signup from "./pages/signup/Signup";
 import DashBoard from "./pages/dashboard/DashBoard";
 import { useContext } from "react";
 import { AuthContext } from "./context/authContext";
+import Loading from "./components/loading/Loading";
+import Users from "./pages/Users/Users";
+import SingleUser from "./pages/singleUser/SingleUser";
+import Groups from "./pages/Groups/Groups";
+import GroupPage from "./pages/GroupPage/GroupPage";
 
 function App() {
-  const { currentUser } = useContext(AuthContext);
+  const { currentUser, loading, setLoading } = useContext(AuthContext);
 
   const PrivateRoutes = () => {
     return currentUser ? <Outlet /> : <Navigate to="/login" />;
@@ -16,10 +21,15 @@ function App() {
 
   return (
     <div className="App">
+      {loading && <Loading />}
       <Router>
         <Routes>
           <Route element={<PrivateRoutes />}>
             <Route path="/" element={<DashBoard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/:username" element={<SingleUser />} />
+            <Route path="/groups" element={<Groups />} />
+            <Route path="/groups/:id" element={<GroupPage />} />
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
