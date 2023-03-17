dae50634e2e845c201919166e88f2f464ab61ab0
jaidan22 <jaidansleeba22@gmail.com>
2023-03-18 00:38:59 +0530

    updated cardpanel

--- client/src/components/cardpanel/CardPanel.js
+++ client/src/components/cardpanel/CardPanel.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import ListPopUp from "../listPopUp/listPopUp";
 import "./cardpanel.scss";
+import OptimisedSettle from "./OptimisedSettle";
 
 const CardPanel = () => {
   const { currentUser, loading, setLoading, logout } = useContext(AuthContext);
@@ -28,19 +29,21 @@ const CardPanel = () => {
         .get(`/owed`)
         .then((res) => {
           setBorrowedData(res.data);
-          console.log(res.data);
+          // console.log(res.data);
         })
         .catch((err) => {
           console.log(err);
           if (err.response.status == 401) logout();
         });
-      setLoading(false)
+      setLoading(false);
     };
     getData();
   }, []);
 
   return (
-    <div className="cardpanel h-7-8 sm:mb-8 sm:mr-10 my-3 mx-2 bg-white rounded-lg ">
+    <div className="cardpanel h-7-8 sm:mb-8 sm:mr-10 my-3 mx-2 p-4 bg-white rounded-lg overflow-y-scroll">
+      <h2 className="text-left font-bold text-3xl">Dashboard</h2>
+
       <ListPopUp
         title="To Recieve"
         open={showLended}
@@ -58,25 +61,31 @@ const CardPanel = () => {
 
       <div className="summary flex sm:flex-row flex-col w-full items-center justify-evenly py-8 sm:mx-0 m-auto gap-4">
         <div
-          className="lended hover:shadow-md flex flex-col justify-center w-56 h-48 bg-green-100 rounded-lg"
+          className="lended hover:shadow-md flex flex-col justify-center sm:w-56 sm:h-48 p-4 h-32 w-52 bg-green-100 rounded-lg"
           onClick={() => showLended(true)}
         >
           <span className="title font-bold text-lg text-green-800">
             Total Lended
           </span>
-          <span className="amt text-green-800 text-5xl mt-4">{lendedData.total}</span>
+          <span className="amt text-green-800 text-5xl mt-4">
+            {lendedData.total}
+          </span>
         </div>
 
         <div
-          className="borrowed hover:shadow-md flex flex-col justify-center w-56 h-48 bg-red-200 rounded-lg"
+          className="borrowed hover:shadow-md flex flex-col justify-center sm:w-56 sm:h-48 p-4 h-32 w-52 bg-red-200 rounded-lg"
           onClick={() => showBorrowed(true)}
         >
           <span className="title text-lg font-bold text-red-800">
             Total Borrowed
           </span>
-          <span className="amt text-red-800 text-5xl mt-4">{borrowedData.total}</span>
+          <span className="amt text-red-800 text-5xl mt-4">
+            {borrowedData.total}
+          </span>
         </div>
       </div>
+
+      <OptimisedSettle />
     </div>
   );
 };
