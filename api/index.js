129867326cf56f7394e02f6670ca776b2c6a0877
jaidan22 <jaidansleeba22@gmail.com>
2023-03-19 20:05:33 +0530

    vercel config

--- api/index.js
+++ api/index.js
@@ -16,21 +16,14 @@ mongoose
   .catch((err) => console.log(err));
 
 //middleware
-app.options("*", (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://wisebillsplit.netlify.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.send();
-});
+app.use(express.json());
+
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://wisebillsplit.netlify.app"
+    "https://wisebillsplit.netlify.app/*"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -47,16 +40,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://wisebillsplit.netlify.app",
-    ],
-  })
-);
 app.use(cookieParser());
 
 app.use("/api", userRoute);
--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
     <div className="min-h-screen bg-gray-200 flex flex-col justify-center py-12 sm:w-full sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-11/12	 sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Logn in
+          Log in
         </h2>
       </div>
 
